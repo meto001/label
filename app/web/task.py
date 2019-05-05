b40f2fd58f412d6870fa3603c03de7f6eef3afbf
meto001 <756246975@qq.com>
2019-05-05 13:43:07 +0800

    修改了标注员获取任务详情为空时的提示

--- app/web/task.py
+++ app/web/task.py
@@ -106,20 +106,22 @@ def show_task_detail():
     # 如果没有锁定数据
     if new_data is None:
         new_data = Task_details().get_new_data(task_id)
-    task_detail_id = new_data.id
-    url = new_data.photo_path
-    prop_ids = new_data.task.prop_ids
+    if new_data:
+        task_detail_id = new_data.id
+        url = new_data.photo_path
+        prop_ids = new_data.task.prop_ids
 
-    prop_ids = list(eval(prop_ids))
-    label_detail = LabelTaskDetailCollection()
-    label_detail.fill(task_id, task_detail_id, url, prop_ids)
+        prop_ids = list(eval(prop_ids))
+        label_detail = LabelTaskDetailCollection()
+        label_detail.fill(task_id, task_detail_id, url, prop_ids)
 
-    # 更新数据，将该条数据锁定
-    with db.auto_commit():
-        new_data.locks = 1
-        new_data.operate_user = user
-    return json.dumps(label_detail, default=lambda o: o.__dict__)
-
+        # 更新数据，将该条数据锁定
+        with db.auto_commit():
+            new_data.locks = 1
+            new_data.operate_user = user
+        return json.dumps(label_detail, default=lambda o: o.__dict__)
+    else:
+        return json.dumps({'status':'该任务已结束'})
 
 @web.route('/task/save_data', methods=['POST'])
 def save_data():
