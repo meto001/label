c660609c8b3e9edf47422c22a4c1b853952d0078
meto001 <756246975@qq.com>
2019-05-05 16:27:24 +0800

    修改了url传输方式，直接传图片流 ------------------- 将base64转换成字符串传输过去

--- app/libs/img_stream.py
+++ app/libs/img_stream.py
@@ -7,7 +7,7 @@ import base64
 
 def return_img_stream(img_local_path):
     img_stream = ''
-    with open(img_local_path,'r') as img_f:
+    with open(img_local_path,'rb') as img_f:
         img_stream = img_f.read()
-        img_stream = base64.b64encode(img_stream)
+        img_stream = base64.b64encode(img_stream).decode(encoding='UTF-8')
     return img_stream
